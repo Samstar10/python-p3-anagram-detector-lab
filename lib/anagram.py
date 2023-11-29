ee33ff6f23e47d422c2961caa2c8c5822ff9ad29
Samuel Muli <samuelmuli95@gmail.com>
2023-11-29 12:30:29 +0300

    Completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, possible_anagrams):
+        return [anagram for anagram in possible_anagrams if sorted(anagram.lower()) == sorted(self.word)]
